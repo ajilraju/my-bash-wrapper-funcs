5b91cdd7529d7805591787d5b11314d7d0558ee1
Ajil Raju <31854172+ajilraju@users.noreply.github.com>
2020-04-24 08:16:00 +0530

    deleted the test script and renamed the setup.sh

--- test/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -o wrapper-funcs https://raw.githubusercontent.com/ajilraju/my-bash-wrapper-funcs/master/wrapper-funcs
-
-if [ -f "./wrapper-funcs" ]; then
-	source ./wrapper-funcs
-fi
