74f7f463e5797411c4ab3568340a9483f81b6ddf
Ajil Raju <31854172+ajilraju@users.noreply.github.com>
2020-04-24 08:40:29 +0530

    Added the unittest for mkdir_cd

--- test/setup_and_test.sh
+++ test/setup_and_test.sh
@@ -1,7 +1,15 @@
+# Script for setup the wrapper-funcs and test each funcation defined in the scripts
+#
 #!/bin/bash
 
+# downloading and sourcing the file
 curl -o wrapper-funcs https://raw.githubusercontent.com/ajilraju/my-bash-wrapper-funcs/master/wrapper-funcs
-
 if [ -f "./wrapper-funcs" ]; then
 	source ./wrapper-funcs
+fi
+
+test_dir='/tmp/test_dir'
+mkdir_cd $test_dir
+if [[ $? -eq 0 && -d $test_dir ]]; then
+	echo "Test Passed..." >> /var/log/wrapper_funcs.log
 fi
